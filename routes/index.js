733c0e8c6dc947a0689d42a1a05ad422fbea0338
gaiyadev <gaiyaobed94@gmail.com>
2020-09-07 09:29:24 +0100

    bug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.json('hello');
+  res.json('hello, this is working');
 });
 
 module.exports = router;
