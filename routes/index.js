521dbaef06dd75280effd29fcbab7e2a5f9ae369
gaiyadev <gaiyaobed94@gmail.com>
2020-09-07 05:29:16 +0100

    Prepared for deployment

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.send('hello');
+router.get('/', (req, res, next) => {
+  res.json('hello');
 });
 
 module.exports = router;
