4e8038dccdda34f7e07be85425a49e196f711e52
gaiyadev <gaiyaobed94@gmail.com>
2020-09-09 04:42:37 +0100

    Code refactoring

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,12 +9,14 @@ var router = express.Router();
     @desc      Sign up a user
     @access    Public
  */
+
 router.post('/register', UserController.sign_up);
 
 /*  @route     POST api/users/login
     @desc      Sign In a user
     @access    Public
  */
+
 router.post('/login', UserController.sign_in);
 
 /*  @route     DELETE api/users/id
@@ -27,18 +29,22 @@ router.delete('/:id', auth, UserController.delete_user);
     @desc      Update user password
     @access    Private
  */
+
 router.patch('/:id', auth, UserController.change_user_password);
 /*  @route     GET api/users/id
     @desc      Get a single user 
     @access    Private
  */
+
 router.get('/:id', auth, UserController.get_user_by_id);
 
 /*  @route     GET api/users/:email
     @desc      CHecking if user email already exits
     @access    Public
  */
+
 router.get('/checkemail/:email', UserController.email_alreadyExist);
+
 /*  @route     GET api/users/:username
     @desc      Checking if username already exits
     @access    Public
